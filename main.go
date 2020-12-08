fd93905f4d9e8b64cdc8c8277e72c3952f3e667d
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-12-08 16:33:33 +0530

    WIP: text message receive
    It seem to be receiving messages, however nothing gets printed. Seem
    like some race condition where the button press makes the async code
    run and the program waiting for the result is not waiting for it
    anymore.

--- main.go
+++ main.go
@@ -21,8 +21,27 @@ func sendText() js.Func {
 	return sendTextFunc
 }
 
+func recvText() js.Func {
+	var recvTextFunc = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid number of arguments"
+		}
+
+		code := args[0].String()
+		var output string
+		go func() {
+			output = RecvText(code)
+		}()
+
+		return output
+	})
+
+	return recvTextFunc
+}
+
 func main() {
-	// make sendText available in JS
+	// make sendText and recvText available in JS
 	js.Global().Set("sendText", sendText())
+	js.Global().Set("recvText", recvText())
 	<-make(chan bool)
 }
---
+++ recvtext.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"fmt"
+
+	"github.com/psanford/wormhole-william/wormhole"
+)
+
+// Code -> Text message
+func RecvText(code string) string {
+	var c = newClient()
+	var ctx = context.Background()
+
+	// todo: verifier support
+	msg, err := c.Receive(ctx, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("returned")
+	switch msg.Type {
+	case wormhole.TransferText:
+		body, err := ioutil.ReadAll(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return string(body)
+	default:
+		return "unsupported transfer type"
+	}
+}
