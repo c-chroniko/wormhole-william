7a0683878dddbf9ca04186f1d69e2094c05a90cb
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2020-12-08 10:19:48 +0530

    call sendText with the value from input text area

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"syscall/js"
+)
+
+func sendText() js.Func {
+	// interface{} can return anything and Go being like C,
+	// different paths can return different values! :-(
+	var sendTextFunc = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid number of arguments"
+		}
+
+		input := args[0].String()
+		go SendText(input)
+
+		return 0
+	})
+
+	return sendTextFunc
+}
+
 func main() {
-	SendText("hello world!")
+	// make sendText available in JS
+	js.Global().Set("sendText", sendText())
+	<-make(chan bool)
 }
