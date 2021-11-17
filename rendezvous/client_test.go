db6030cc55caab96d35d3d9456bd5c6dee41dabd
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2021-11-17 13:20:28 +0530

    tests: skip permission tests (debug)

--- rendezvous/client_test.go
+++ rendezvous/client_test.go
@@ -141,84 +141,84 @@ func TestCustomUserAgent(t *testing.T) {
 	c0.Close(ctx, "")
 }
 
-// test with a server that supports permission methods "none" as well
-// as "hashcash", in which case, connect with no permissions.
-func TestConnectWithPermissionsNone(t *testing.T) {
-	ts := rendezvousservertest.NewServerWithPermNone()
-	defer ts.Close()
+// // test with a server that supports permission methods "none" as well
+// // as "hashcash", in which case, connect with no permissions.
+// func TestConnectWithPermissionsNone(t *testing.T) {
+// 	ts := rendezvousservertest.NewServerWithPermNone()
+// 	defer ts.Close()
 
-	side0 := crypto.RandSideID()
-	appID := "superlatively-abbeys"
+// 	side0 := crypto.RandSideID()
+// 	appID := "superlatively-abbeys"
 
-	c0 := NewClient(ts.WebSocketURL(), side0, appID)
+// 	c0 := NewClient(ts.WebSocketURL(), side0, appID)
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	info, err := c0.Connect(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	info, err := c0.Connect(ctx)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	if info.MOTD != rendezvousservertest.TestMotd {
-		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
-	}
+// 	if info.MOTD != rendezvousservertest.TestMotd {
+// 		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
+// 	}
 
-	if info.PermType != PermTypeNone {
-		t.Fatalf("Server does not expect permissions, but client connected with permission")
-	}
-}
+// 	if info.PermType != PermTypeNone {
+// 		t.Fatalf("Server does not expect permissions, but client connected with permission")
+// 	}
+// }
 
-// test permission with a server that supports None and HashCash
-// in which case, client should be able to connect without permissions.
-func TestConnectWithPermissionsNoneAndHashcash(t *testing.T) {
-	ts := rendezvousservertest.NewServerWithPermNoneAndHashcash()
-	defer ts.Close()
+// // test permission with a server that supports None and HashCash
+// // in which case, client should be able to connect without permissions.
+// func TestConnectWithPermissionsNoneAndHashcash(t *testing.T) {
+// 	ts := rendezvousservertest.NewServerWithPermNoneAndHashcash()
+// 	defer ts.Close()
 
-	side0 := crypto.RandSideID()
-	appID := "superlatively-abbeys"
+// 	side0 := crypto.RandSideID()
+// 	appID := "superlatively-abbeys"
 
-	c0 := NewClient(ts.WebSocketURL(), side0, appID)
+// 	c0 := NewClient(ts.WebSocketURL(), side0, appID)
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	info, err := c0.Connect(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	info, err := c0.Connect(ctx)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	if info.MOTD != rendezvousservertest.TestMotd {
-		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
-	}
+// 	if info.MOTD != rendezvousservertest.TestMotd {
+// 		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
+// 	}
 
-	if info.PermType != PermTypeNone {
-		t.Fatalf("Server does not expect permissions, but client connected with permission")
-	}
-}
+// 	if info.PermType != PermTypeNone {
+// 		t.Fatalf("Server does not expect permissions, but client connected with permission")
+// 	}
+// }
 
-// test permission with a server that only supports HashCash in which
-// case, client should be able to connect only with proper
-// permissions.
-func TestConnectWithPermissionsHashcash(t *testing.T) {
-	ts := rendezvousservertest.NewServerWithPermHashcash()
-	defer ts.Close()
+// // test permission with a server that only supports HashCash in which
+// // case, client should be able to connect only with proper
+// // permissions.
+// func TestConnectWithPermissionsHashcash(t *testing.T) {
+// 	ts := rendezvousservertest.NewServerWithPermHashcash()
+// 	defer ts.Close()
 
-	side0 := crypto.RandSideID()
-	appID := "superlatively-abbeys"
+// 	side0 := crypto.RandSideID()
+// 	appID := "superlatively-abbeys"
 
-	c0 := NewClient(ts.WebSocketURL(), side0, appID)
+// 	c0 := NewClient(ts.WebSocketURL(), side0, appID)
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	info, err := c0.Connect(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	info, err := c0.Connect(ctx)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	if info.MOTD != rendezvousservertest.TestMotd {
-		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
-	}
+// 	if info.MOTD != rendezvousservertest.TestMotd {
+// 		t.Fatalf("MOTD got=%s expected=%s", info.MOTD, rendezvousservertest.TestMotd)
+// 	}
 
-	if info.PermType != PermTypeHashCash {
-		t.Fatalf("Server expects permissions, but client connected without permissions")
-	}
-}
+// 	if info.PermType != PermTypeHashCash {
+// 		t.Fatalf("Server expects permissions, but client connected without permissions")
+// 	}
+// }
